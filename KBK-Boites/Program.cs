b4a15d79e160a166cc7c83584516f413ae361599
Yannick Turpin <202066200@edu.clg.qc.ca>
2024-02-16 15:20:57 -0500

    Initial import

---
+++ KBK-Boites/Program.cs
@@ -0,0 +1,10 @@
+﻿namespace KBK_Boites
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello, World!");
+        }
+    }
+}
